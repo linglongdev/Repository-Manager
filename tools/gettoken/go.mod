a71134410ddafcafa1698e37d195a3312b2bc6f8
myml <wurongjie@deepin.org>
2024-08-19 11:26:13 +0800

    feat: add gettoken binary

---
+++ tools/gettoken/go.mod
@@ -0,0 +1,12 @@
+module gettoken
+
+go 1.22.1
+
+require github.com/bradleyfalzon/ghinstallation v1.1.1
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/google/go-github/v29 v29.0.2 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+)
---
+++ tools/gettoken/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/bradleyfalzon/ghinstallation"
+)
+
+func main() {
+	var GitHubAppID = os.Getenv("MK_REPO_APP_ID")
+	var GitHubAppInstallID = os.Getenv("MK_REPO_APP_INSTALL_ID")
+	var GitHubAppPrivateKey = os.Getenv("MK_REPO_APP_PRIVATE_KEY")
+	token, err := getToken(GitHubAppID, GitHubAppInstallID, GitHubAppPrivateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	print(*token)
+}
+
+func getToken(app_id, app_install_id, app_private_key string) (*string, error) {
+	id, err := strconv.ParseInt(app_id, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("parse app id: %w", err)
+	}
+	installID, err := strconv.ParseInt(app_install_id, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("parse app install id: %w", err)
+	}
+	itr, err := ghinstallation.NewKeyFromFile(http.DefaultTransport, id, installID, app_private_key)
+	if err != nil {
+		return nil, fmt.Errorf("init ghinstallation: %w", err)
+	}
+	token, err := itr.Token(context.Background())
+	return &token, err
+}
